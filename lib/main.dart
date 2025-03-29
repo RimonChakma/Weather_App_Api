2bfd24fe68cfd4fb8290b68a364633b97406c567
RimonChakma <rimon.chakma880@gmail.com>
2025-03-29 22:01:41 +0600

    weather screen

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WeatherUiScreen(),
+    );
+  }
+}
+
+class WeatherUiScreen extends StatefulWidget {
+  const WeatherUiScreen({super.key});
+
+  @override
+  State<WeatherUiScreen> createState() => _WeatherUiScreenState();
+}
+
+class _WeatherUiScreenState extends State<WeatherUiScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Weather App"),centerTitle: true,),
+
+    );
+  }
+}
+
