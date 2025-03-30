cdf3a87449a7a89d2b123db82a2be27aedc54c84
RimonChakma <rimon.chakma880@gmail.com>
2025-03-30 22:00:50 +0600

    add weather list item variable

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,36 @@ class WeatherUiScreen extends StatefulWidget {
 }
 
 class _WeatherUiScreenState extends State<WeatherUiScreen> {
+
+  String temperature= "Temperature:";
+  String city = "City:";
+  String feelsLike = "fellsLike:";
+  String windSpeed ="WindSpeed:";
+  String humidity = "humidity:";
+  String visibility = "visibility:";
+  String pressure = 'pressure';
+  String icon = "icon:";
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Weather App"),centerTitle: true,),
       body: Padding(padding: EdgeInsets.all(10),child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextFormField(decoration: InputDecoration(
       ),),
-    ElevatedButton(onPressed: (){}, child: Text("Get data"))
+      ElevatedButton(onPressed: (){}, child: Text("Get data")),
+            SizedBox(height: 10,),
+            Text(temperature,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+            Text(city,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+            Text(feelsLike,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+            Text(windSpeed,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+            Text(humidity,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+            Text(visibility,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+            Text(icon,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+            Text(pressure,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
         ],
       ),),
 
